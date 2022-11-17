1ac84642d75c6a089ff1579c4ea6c2b5fbd76567
tuannguyen0708 <ndtuan.bkdn@gmail.com>
2022-11-17 13:12:59 +0700

    update validator

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { Component, OnInit} from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
 import {ProductListModal} from "../product-list/product-list.modal";
 import {CartService} from "../service/cart.service";
@@ -9,13 +9,13 @@ import {CartService} from "../service/cart.service";
   templateUrl: './cart.component.html',
 })
 export class CartComponent implements OnInit {
-  dataCart = require('../../assets/cart.json');
+  dataCart: ProductListModal[] = [];
   arrTotal: number[] = [];
   total: number = 0;
   submitForm: FormGroup = this.fb.group({
     fullName: ['', [Validators.required, Validators.minLength(5)]],
     address: ['', [Validators.required, Validators.minLength(5)]],
-    creditCard: ['', [Validators.required, Validators.minLength(16), Validators.maxLength(16)]],
+    creditCard: ['', [Validators.required, Validators.minLength(16), Validators.maxLength(16)]]
   })
 
   constructor(private fb: FormBuilder, private router: Router, private cartService: CartService) {}
@@ -31,8 +31,10 @@ export class CartComponent implements OnInit {
 
   getTotal() {
     this.arrTotal = [];
-    for (let i = 0; i < this.dataCart.length; i++) {
-      this.arrTotal.push(this.dataCart[i].amount * this.dataCart[i].price);
+    if (this.dataCart.length > 0) {
+      for (let i = 0; i < this.dataCart.length; i++) {
+        this.arrTotal.push(this.dataCart[i].amount! * this.dataCart[i].price);
+      }
     }
     return this.total = this.arrTotal.length > 0 ? this.arrTotal.reduce((a: number, b: number) => a + b) : 0;
   }
@@ -40,27 +42,25 @@ export class CartComponent implements OnInit {
   changeAmount(data: any, product: ProductListModal) {
     const amount = Number(data.value)
     const findCart = this.dataCart.find((item:ProductListModal) => item.id === product.id);
-    const index = this.dataCart.indexOf(findCart)
-    if (findCart) {
+    if (findCart && amount >= 1) {
       findCart.amount = amount
       this.getTotal()
-    }
-    if (amount < 1) {
-      this.dataCart.splice(index,1)
     }
   }
 
   submit() {
     console.log(this.submitForm)
-    // localStorage.setItem("total", this.total.toString());
-    // localStorage.setItem("name", this.submitForm.get('fullName')?.value);
-    // this.router.navigate(['/payment']).then()
+    localStorage.setItem("total", this.total.toString());
+    localStorage.setItem("name", this.submitForm.get('fullName')?.value);
+    this.router.navigate(['/payment']).then()
   }
 
   delete(id: number) {
     const product = this.dataCart.find((item: ProductListModal) => item.id === id)
-    const index = this.dataCart.indexOf(product)
-    this.dataCart.splice(index,1)
+    if (product) {
+      const index = this.dataCart.indexOf(product)
+      this.dataCart.splice(index,1)
+    }
     this.getTotal()
     alert('Delete product success')
   }
